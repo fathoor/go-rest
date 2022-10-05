165520ac38a03062f36b35bd70237081edb46c59
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-05 18:18:44 +0700

    add: model

---
+++ models/model.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Order struct {
+	Id           int       `json:"orderId" gorm:"primaryKey"`
+	CustomerName string    `json:"customerName" gorm:"type:varchar(191);not null"`
+	OrderedAt    time.Time `json:"orderedAt"`
+	Items        []Item    `json:"items"`
+}
+
+type Item struct {
+	Id          int    `json:"itemId" gorm:"primaryKey"`
+	ItemCode    string `json:"itemCode" gorm:"type:varchar(191);not null"`
+	Description string `json:"description" gorm:"type:varchar(191);not null"`
+	Quantity    int    `json:"quantity" gorm:"type:int;not null"`
+	OrderId     int
+}
