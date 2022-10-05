344093070150883edabce84320dff88ea6f4bc56
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-05 19:54:40 +0700

    edit: re-arranged and commented values

--- models/model.go
+++ models/model.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type Order struct {
-	Id           int       `json:"orderId" gorm:"primaryKey"`
-	CustomerName string    `json:"customerName" gorm:"type:varchar(191);not null"`
+	Id           int       `json:"-" gorm:"primaryKey"`
 	OrderedAt    time.Time `json:"orderedAt"`
+	CustomerName string    `json:"customerName" gorm:"type:varchar(191);not null"`
 	Items        []Item    `json:"items" gorm:"foreignKey:OrderId"`
 }
 
 type Item struct {
-	Id          int    `json:"itemId" gorm:"primaryKey"`
+	Id          int    `json:"-" gorm:"primaryKey"`
 	ItemCode    string `json:"itemCode" gorm:"type:varchar(191);not null"`
 	Description string `json:"description" gorm:"type:varchar(191);not null"`
 	Quantity    int    `json:"quantity" gorm:"type:int;not null"`
-	OrderId     int
+	OrderId     int    `json:"-"`
 }
