4730b512892994fe4058c77545635ae8994ddc59
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-05 18:20:17 +0700

    add: config with `godotenv/autoload`

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"os"
+
+	_ "github.com/joho/godotenv/autoload" // Auto-load .env file
+)
+
+type config struct {
+	// Database
+	DB_HOST     string
+	DB_PORT     string
+	DB_USER     string
+	DB_PASS     string
+	DB_NAME     string
+	DB_SSL_MODE string
+
+	// Server
+	SERVER_HOST string
+	SERVER_PORT string
+}
+
+var Config = config{
+	DB_HOST:     os.Getenv("DB_HOST"),
+	DB_PORT:     os.Getenv("DB_PORT"),
+	DB_USER:     os.Getenv("DB_USER"),
+	DB_PASS:     os.Getenv("DB_PASS"),
+	DB_NAME:     os.Getenv("DB_NAME"),
+	DB_SSL_MODE: os.Getenv("DB_SSL_MODE"),
+	SERVER_HOST: os.Getenv("SERVER_HOST"),
+	SERVER_PORT: os.Getenv("SERVER_PORT"),
+}
