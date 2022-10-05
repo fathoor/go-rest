304440bd94f09d19ce87fe8f963dd9f5cbcca711
fathoor <fathurrahman.205026@mhs.its.ac.id>
2022-10-05 19:55:47 +0700

    edit: added `GetOrders` func

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,7 +1,20 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-rest/config"
+	"go-rest/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetOrders(c *gin.Context) {
-	println("GetOrders")
+	DB := config.FetchDB()
+
+	orders := []models.Order{}
+	if err := DB.Preload("Items").Find(&orders).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"orders": orders})
+	}
 }
